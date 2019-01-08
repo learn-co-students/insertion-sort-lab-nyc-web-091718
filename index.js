a3503dd12e9acdf332ffd031ca792fa965164cbc
AsafDavidov <adavidov@conncoll.edu>
2019-01-08 11:28:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function findMinAndRemove(array){
-
+  let minIndex = array.indexOf(Math.min(...array))
+  return array.splice(minIndex,1)
 }
 
 function insertionSort(array){
-
+  let sortedArray = [];
+  const originalLength = array.length
+  for (let i = 0; i < originalLength; i++) {
+    sortedArray.push(findMinAndRemove(array)[0])
+  }
+  return sortedArray
 }
